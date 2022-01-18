5f00e5f111018dc5f56ac3da55e918dd95484b04
Laura Gao <lauragao86868@gmail.com>
2022-01-18 15:39:25 -0500

    Use more efficient mongodb query with $or

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -11,26 +11,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
         await dbConnect();
 
-        const fileLookup = [{
-            $lookup: {
-                from: "files",
-                localField: "file", // Section field
-                foreignField: "_id", //  File field
-                as: "fileArr",
+        const allMatchingSections = await SectionModel.aggregate([
+            {$match: {$or: [
+                {"body": {$regex: `.*${req.query.query}.*`, $options: "i"}}, 
+                {"name": {$regex: `.*${req.query.query}.*`, $options: "i"}}
+            ]}},
+            {
+                $lookup: {
+                    from: "files",
+                    localField: "file", // Section field
+                    foreignField: "_id", //  File field
+                    as: "fileArr",
+                },
             },
-        }]
-
-        // const allMatchingSections = await SectionModel.find({$text: {$search: req.query.query}})
-        let allMatchingSections;
-        const matchingBodySections = await SectionModel.aggregate([
-            {$match: {"body": {$regex: `.*${req.query.query}.*`, $options: "i"}}},
-            ...fileLookup,
         ])
-        const matchingNameSections = await SectionModel.aggregate([
-            {$match: {"name": {$regex: `.*${req.query.query}.*`, $options: "i"}}},
-            ...fileLookup,
-        ])
-        allMatchingSections = matchingBodySections.concat(matchingNameSections)
 
         // If return 404, data will be undefined. so u dont have to check that data.data.length !== 0
         if (!allMatchingSections || !allMatchingSections.length) return res.status(404).send("No documents with this query were found")
