cb40293bb4edfa339c854b968409079fef7bba56
Laura Gao <lauragao86868@gmail.com>
2022-01-18 21:58:32 -0500

    Update error pages to reflect no navbar

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,21 +1,17 @@
-import Container from '../components/Container'
-import H2 from '../components/H2'
 import { A } from './index'
+import { NextJSCopyErrorComponent } from "./_error"
 
-const custom404 = () => {
-    return (
-        <Container className="flex items-center justify-center text-center" style={{height: "calc(100vh - 98px)"}}>
-            <div>
-                <div className="flex items-center justify-center mb-10">
-                    <div className="border-r border-black pr-4 mr-4 h-20 flex items-center">
-                        <H2 className="">404</H2>
-                    </div>
-                    <p>This page could not be found.</p>
-                </div>
-                <p>Think this is a mistake? Contact me @ gaolauro@gmail.com or make an issue on <A href="https://github.com/laurgao/scratchpad">GitHub</A></p>
-            </div>
-        </Container>
-    )
-}
+// fyi without this file, nextjs'd use _error.tsx with statusCode=404
+const custom404 = () => (
+    <NextJSCopyErrorComponent
+        statusCode={404}
+        message="This page could not be found."
+        message2={
+            <p>Think this is a mistake? Contact me @ gaolauro@gmail.com or make an issue on{" "}
+                <A href="https://github.com/laurgao/scratchpad">GitHub</A>
+            </p>
+        }
+    />
+)
 
 export default custom404
